0ece55d1a7ac9276d43dbd53df6561513c9a5959
T109154 <T109154@DCSCOB-BQGJM53.County.Allegheny.Local>
2021-08-15 19:39:39 -0400

    Test commit

--- NorthwoodsKata/src/NorthwoodsKata.java
+++ NorthwoodsKata/src/NorthwoodsKata.java
@@ -1,4 +1,8 @@
 
 public class NorthwoodsKata {
-
+public static void main(String[] args) {
+	//creating main void to testing commits. after 5 long hours of configuring GIT for Eclipse. I can finally push changes.
+	
+}
+	
 }
