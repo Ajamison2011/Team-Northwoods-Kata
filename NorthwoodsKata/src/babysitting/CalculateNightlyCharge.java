6de3c4ff628ef91808581b919c200e8968197e16
T109154 <T109154@DCSCOB-BQGJM53.County.Allegheny.Local>
2021-08-16 10:56:54 -0400

    Failed attempt.

--- NorthwoodsKata/src/babysitting/CalculateNightlyCharge.java
+++ NorthwoodsKata/src/babysitting/CalculateNightlyCharge.java
@@ -5,23 +5,24 @@ import java.time.format.DateTimeFormatter;
 
 public class CalculateNightlyCharge {
 
-	public boolean checkStartTime(int time) {
-		
+	public boolean checkStartTime(String time) {
 		LocalTime startTimeStart = LocalTime.parse("17:00:00");
 		LocalTime endtime = LocalTime.parse("04:00:00");
-	
-				for(LocalTime startTime = LocalTime.parse("17:00:00");startTime.isAfter(startTimeStart);startTime.plusHours(1)) {
-					System.out.println(startTime);
-					
-				}
-				
-	
-		if (time >= 5) {
+		LocalTime userTime = LocalTime.now();
+		
+		if(userTime.isAfter(startTimeStart)) {
 			return true;
-		} else {
+		}else {
 			return false;
 		}
+		
 	}
+		
+	
+	//			for(LocalTime startTime = LocalTime.parse("17:00:00");startTime.isAfter(startTimeStart);startTime.plusHours(1)) {
+	//				System.out.println(startTime);
+					
+		//		}
 
 	public boolean checkEndTime(int time) {
 		if (time >= 4) {
--- NorthwoodsKata/test/babysitting/CalculateNightlyChargeTest.java
+++ NorthwoodsKata/test/babysitting/CalculateNightlyChargeTest.java
@@ -7,28 +7,28 @@ public class CalculateNightlyChargeTest {
 	@Test
 	public void shouldCheckIsTimeIsAfter5Pm() {
 		CalculateNightlyCharge calculate = new CalculateNightlyCharge();
-		boolean result = calculate.checkStartTime(5);
+		boolean result = calculate.checkStartTime("");
 		assertTrue(result);
 	}
 	
 	@Test 
 	public void shouldCheckIfTimeIsBefore5Pm() {
 		CalculateNightlyCharge calculate = new CalculateNightlyCharge();
-		boolean result = calculate.checkStartTime(4);
+		boolean result = calculate.checkStartTime("");
 		assertFalse(result);
 	}
 	
 	@Test
 	public void shouldCheckIfEndTimeIsLessThan4Am() {
 		CalculateNightlyCharge calculate = new CalculateNightlyCharge();
-		boolean result = calculate.checkEndTime(3);
+		boolean result = calculate.checkEndTime(6);
 		assertTrue(result);
 	}
 	
 	@Test
 	public void shouldCheckIfEndTimeIsGreaterThan4Am() {
 		CalculateNightlyCharge calculate = new CalculateNightlyCharge();
-		boolean result = calculate.checkEndTime(6);
+		boolean result = calculate.checkEndTime(3);
 		assertFalse(result);
 	}
 	
