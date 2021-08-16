6afbcac07cf7167b780936b0c1f3a7407255556c
T109154 <T109154@DCSCOB-BQGJM53.County.Allegheny.Local>
2021-08-16 09:52:23 -0400

    find a more logical way of checking startTime

--- NorthwoodsKata/src/babysitting/CalculateNightlyCharge.java
+++ NorthwoodsKata/src/babysitting/CalculateNightlyCharge.java
@@ -1,8 +1,16 @@
 package babysitting;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class CalculateNightlyCharge {
 
 	public boolean checkStartTime(int time) {
+		
+	LocalTime localTime = LocalTime.now();
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+	System.out.println(localTime.format(formatter));
+	
 		if (time >= 5) {
 			return true;
 		} else {
